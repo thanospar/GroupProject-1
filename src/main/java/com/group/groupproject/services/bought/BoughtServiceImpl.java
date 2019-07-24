3e7804d8a4742776cbb7fe693092d0d1d6ffe062
Dimitris Tsiktsiris <ditsikts@outlook.com>
2019-07-24 21:19:03 +0300

    boughtService3

--- src/main/java/com/group/groupproject/services/bought/BoughtServiceImpl.java
+++ src/main/java/com/group/groupproject/services/bought/BoughtServiceImpl.java
@@ -6,6 +6,7 @@ import com.group.groupproject.entities.Bought;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service("boughtService")
 public class BoughtServiceImpl implements BoughtService{
@@ -14,26 +15,31 @@ public class BoughtServiceImpl implements BoughtService{
     BoughtDao boughtDao;
     
     @Override
+    @Transactional
     public Bought findById(int id) {
         return boughtDao.findById(id);
     }
 
     @Override
+    @Transactional
     public List<Bought> findAllBoughts() {
         return boughtDao.findAllBoughts();
     }
 
     @Override
+    @Transactional
     public boolean saveBought(Bought bought) {
         return boughtDao.saveBought(bought);
     }
 
     @Override
+    @Transactional
     public boolean updateBought(Bought bought) {
         return boughtDao.updateBought(bought);
     }
 
     @Override
+    @Transactional
     public boolean deleteBought(Bought bought) {
         return boughtDao.deleteBought(bought);
     }
